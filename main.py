f4f0ab47589762dda5bddd14e29c42c4a9d0c94f
Rachel Coleman <rachelelise527@gmail.com>
2021-03-13 16:29:28 -0700

    take code from vm and push to github

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+from OpenSSL import crypto
+import os
+import sys
+import datetime
+import whois
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#Variables
+TYPE_RSA = crypto.TYPE_RSA
+cn = input("Common Name: ")
+key = crypto.PKey()
+req = crypto.X509Req()
+dir_path = os.path.dirname(os.path.realpath(__file__))
+keypath = dir_path + "/" + cn.replace(".", "_") + ".key"
+csrpath = dir_path + "/" + cn.replace(".", "_") + ".csr"
 
+#create private key
+def generatekey():
+    #set key size
+    print("Generating private key.")
+    keysize = input("Key size[2048]: ")
+    if not keysize:
+        keysize = 2048
+    keysize = int(keysize)
+    key.generate_key(TYPE_RSA, keysize)
+    keyfile = open(keypath, "wb")
+    keyfile.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
+    keyfile.close()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+generatekey()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#create CSR
+def generatecsr():
